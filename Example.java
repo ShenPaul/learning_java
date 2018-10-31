3594fdd2863b6eb2c980144c6aa57a1e90a80610
Paul Shen <23107391+ShenPaul@users.noreply.github.com>
2018-10-31 03:16:59 -0400

    Delete Example.java

--- Example.java
+++
@@ -1,9 +0,0 @@
-public class Example{
-	public static void main(String []args){
-		
-		int s=0;	
-		for (int i=0;i<args.length;i++)
-			s+=Integer.parseInt(args[i]);
-		System.out.println(s);
-	}
-}
